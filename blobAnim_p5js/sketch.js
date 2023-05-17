0cde2adcd1a2790e91fc01a35f939a5257e39f18
ndr3svt <andresvillatorres@gmail.com>
2023-05-17 12:45:56 +0200

    modify centering and scale

--- blobAnim_p5js/sketch.js
+++ blobAnim_p5js/sketch.js
@@ -25,9 +25,9 @@ function positions(){
       let y = index % width;
       let x = Math.floor(index / width);
       
-      let freq = getRandomFloat(0.005,0.15);
+      let freq = getRandomFloat(0.01,0.015);
+      // let freq = getRandomFloat(0.1,0.1);
       if(x % 2 !== 0 && x!==0){
-        
         let sW = blobSmall.width/scaleFactorBlob*1.5;
         let sH = blobSmall.height/scaleFactorBlob*1.45;
         let offsetX = (blobSmall.width/scaleFactorBlob*0.5);
@@ -56,84 +56,45 @@ function draw() {
 
 
   translate(124/mainScaleFactor,124/mainScaleFactor);
+  /* drawing first row */
   for(let i =0; i< tBlobs.length-1;i++){
     let y = i % 3;
     let x = Math.floor(i/6);
     
+    if(y==0){
+      tBlobs[i].display();
+    }
+  }
+
+  /* drawing second row */
+  for(let i = 0; i<tBlobs.length-1;i++){
+    let y = i % 3;
+    let x = Math.floor(i/6);
     if(y % 2 !== 0 ){
       push();
       rotate(radians(180));
       translate(-1672/mainScaleFactor,- 586/mainScaleFactor )
       tBlobs[i].display();
       pop();
-    }else{
+    }
+  }
+
+  /* drawing third row */
+  for(let i = 0; i<tBlobs.length-1;i++){
+    let y = i % 3;
+    let x = Math.floor(i/6);
+    if(y==2){
       tBlobs[i].display();
     }
-
   }
+  
   debugAnchors();
   pop();
   printMouse();
  
 }
 
-class Blob{
-  constructor(_x, _y, _w,_h,_type, _freq){
-    this.x = _x;
-    this.y = _y;
-    this.ox = 0;
-    this.oy = 0;
-    this.type = _type;
-    if(_type == "small"){
-      this.blobImg = blobSmall;
-    }else if(_type =="small_mirrored"){
-      this.blobImg = blobSmallMirr;
-    }
-    this.ow = _w;
-    this.oh = _h;
-    this.w = _w;
-    this.h = _h;
-    this.oscOpacity=255;
-    this.time = 0;
-    this.freq= _freq*getRandomFloat(0.5,0.25);
-    this.time2=0;
-    this.freq2 = _freq*getRandomFloat(0.15,0.5);
-  }
 
-  display(){
-    tint(255,this.oscOpacity);
-    image(this.blobImg, this.x+this.ox, this.y + this.oy, this.w, this.h);
-   
-    this.update();
-  }
-
-  displayAnchor(){
-    let x = this.x + this.ox;
-    let y = this.y + this.oy;
-    tint(255,255);
-    noStroke();
-    fill(255,0,0);
-    ellipse(x,y,2,2);
-    stroke(0,255,0);
-    strokeWeight(0.5);
-    noFill();
-    line(x-15,y,x+15,y)
-    line(x,y-15,x,y+15)
-    strokeWeight(0.5);
-    stroke(255,255,0);
-    rect(x,y,this.w,this.h)
-  }
-  update(){
-    this.time+=this.freq;
-    this.time2+=this.freq2;
-    this.oscOpacity=map(Math.sin(this.time), -1,1,155,255);
-    this.ox = map(Math.sin(this.time2), -1,1,-8,8);
-    this.oy = map(Math.sin(this.time), -1,1,-10,10);
-    this.w = map(Math.sin(this.time), -1,1, this.ow-20,this.ow+20);
-    this.h = map(Math.sin(this.time2), -1,1, this.oh-10,this.oh+10);
-
-  }
-}
 
 function keyPressed() {
   if (key === 'a' || key === 'A') {
@@ -175,9 +136,82 @@ function debugAnchors(){
 
 function toggleDebugAnchors() {
   if(document.getElementById("myCheckbox").checked) {
-    // alert("Checkbox is checked!");
+
     toggleDebug=true;
   }else{
     toggleDebug=false;
+  }
+}
+
+
+
+
+class Blob{
+  constructor(_x, _y, _w,_h,_type, _freq){
+    this.offsetx=0;
+    this.offsety=0;
+    this.x = _x;
+    this.y = _y;
+    this.ox = 0;
+    this.oy = 0;
+    this.type = _type;
+    if(_type == "small"){
+      this.blobImg = blobSmall;
+    }else if(_type =="small_mirrored"){
+      this.blobImg = blobSmallMirr;
+    }
+    this.ow = _w;
+    this.oh = _h;
+    this.w = _w;
+    this.h = _h;
+    this.scale =1;
+    this.oscOpacity=255;
+    this.time = 0;
+    this.freq= _freq*getRandomFloat(0.5,0.5);
+    this.time2=0;
+    this.freq2 = _freq*getRandomFloat(0.5,0.5);
+  }
+
+  display(){
+    // tint(255,this.oscOpacity);
+    tint(255,255)
+    image(this.blobImg, this.x+this.offsetx+this.ox, this.y+this.offsety + this.oy, this.w, this.h);
+   
+    this.update();
+  }
+
+  displayAnchor(){
+    let x = this.x + this.ox;
+    let y = this.y + this.oy;
+    tint(255,255);
+    noStroke();
+    fill(255,0,0);
+    ellipse(x,y,2,2);
+    stroke(0,255,0);
+    strokeWeight(0.5);
+    noFill();
+    line(x-15,y,x+15,y)
+    line(x,y-15,x,y+15)
+    strokeWeight(0.5);
+    stroke(255,255,0);
+    rect(x,y,this.w,this.h)
+  }
+  update(){
+    this.time+=this.freq;
+    this.time2+=this.freq;
+    
+    this.oscOpacity=map(Math.sin(this.time), -1,1,155,255);
+
+    /* play with the scale */
+    // this.ox = map(Math.sin(this.time2), -1,1,0.9,1.1);
+    // this.oy = map(Math.sin(this.time), -1,1,-10,10);
+    // this.w = map(Math.sin(this.time), -1,1, this.ow*this.ox,this.ow*this.ox);
+    this.scale =map(Math.sin(this.time2), -1,1,0.85,1.15);
+    this.w = this.ow*this.scale;
+    this.h = this.oh*this.scale;
+    this.offsetx = (this.ow-this.w)/2;
+    this.offsety = (this.ow-this.w)/2;
+    // this.h = map(Math.sin(this.time2), -1,1, this.oh-10,this.oh+10);
+
   }
 }
